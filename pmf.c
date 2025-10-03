2ecfa90194480c3931a90b9e8103288fb30e313e
Brady <bradybbangasser@gmail.com>
2025-10-02 19:37:15 -0500

    Prompting user for code

--- pmf.c
+++ pmf.c
@@ -2,7 +2,7 @@
 #include <security/pam_ext.h>
 #include <security/pam_modules.h>
 
-#include <stdio.h>
+#include <stdlib.h>
 
 PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
                               const char **argv) {
@@ -11,8 +11,28 @@ PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
                                    const char **argv) {
-  pam_info(pamh, "You");
-  pam_error(pamh, "Dumbass");
+  const struct pam_conv *conv = NULL;
+  struct pam_message msg = {0};
+  const struct pam_message *msgp = &msg;
+  struct pam_response *resp = NULL;
+
+  if (pam_get_item(pamh, PAM_CONV, (const void **)&conv) != PAM_SUCCESS) {
+    pam_error(pamh, "PAM Auth failure, failed to get conversation");
+    return PAM_CONV_ERR;
+  }
+
+  msg.msg = "Enter FC: ";
+  msg.msg_style = PAM_PROMPT_ECHO_OFF;
+
+  if (conv->conv(1, &msgp, &resp, conv->appdata_ptr) != PAM_SUCCESS || !resp ||
+      !resp->resp) {
+    if (resp)
+      free(resp);
+    pam_error(pamh, "PAM Auth failure, failed to get FC");
+    return PAM_CONV_ERR;
+  }
+
+  pam_info(pamh, "'%s'", resp->resp);
 
   return PAM_SUCCESS;
 }
